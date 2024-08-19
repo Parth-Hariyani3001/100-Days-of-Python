a49cce0aab7fe55a63d6d2dda56aa97925955c12
Parth Hariyani <parth.hariyani@percipere.co>
2024-08-19 13:29:00 +0530

    Day 5

---
+++ Day 5 - Python Loops/main.py
@@ -0,0 +1,17 @@
+# For loops
+# fruits = ['Apple','Peach','Pear']
+# for fruit in fruits:
+#     print(fruit)
+
+# Highest score challenge
+# student_scores = [150, 250, 89, 89, 32, 43, 123, 32, 43,42]
+# print(sum(student_scores))
+# print(max(student_scores))
+# print(min(student_scores))
+
+# Range method in loops
+sum = 0
+for i in range(1,101):
+    sum += i
+
+print(sum)
