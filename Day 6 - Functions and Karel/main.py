e3f3a5389de7bba7b64fe4a2a4f8f897c91f15ad
Parth Hariyani <parth.hariyani@percipere.co>
2024-08-20 13:48:42 +0530

    Day 6

---
+++ Day 6 - Functions and Karel/main.py
@@ -0,0 +1,38 @@
+# Function
+def my_function():
+    print("Hello")
+    print("bye")
+
+my_function()
+
+
+#while loop
+i = 5
+while(i > 0):
+    print(i)
+    i -= 1
+
+# Reebog's world code 
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()    
+    
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#         turn_right()
+#     elif wall_in_front():
+#         turn_left()
+#front_is_clear() or wall_in_front(), at_goal(),
+
